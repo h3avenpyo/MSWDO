46c8e73a0878f1f324f55f72d9f0ea7a21bf1af1
Gerald Louis <geraldlouissumaylo@gmail.com>
2026-08-17 23:25:04 +0800

    hehe

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -686,10 +686,7 @@
 
         /* ---------- Footer ---------- */
         .footer {
-            position: absolute;
-            left: 0;
-            right: 0;
-            bottom: 0;
+            margin-top: 1.5rem;
             text-align: center;
             font-size: .7813rem;
             font-weight: 500;
