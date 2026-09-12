2546cc0aa1c399ef72415793f070f589d9b0de46
Gerald Louis <geraldlouissumaylo@gmail.com>
2026-09-12 21:40:58 +0800

    fix logout social case

--- resources/views/admin/social-case/layout.blade.php
+++ resources/views/admin/social-case/layout.blade.php
@@ -2556,6 +2556,7 @@
             text: 'Do you really want to log out?',
             icon: 'warning',
             showCancelButton: true,
+            reverseButtons: true,
             confirmButtonColor: '#1A237E',
             cancelButtonColor: '#EF4444',
             confirmButtonText: 'Yes, log out',
